026b7b0dbd4225bf055268f735634b1175cbc5b3
mehadi <mehadishisir@gmail.com>
2026-02-15 11:02:48 +0600

    delete user and update ui

--- src/component/Users.jsx
+++ src/component/Users.jsx
@@ -1,32 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 
 const Users = () => {
   const initialData = useLoaderData();
+  const [users, setUsers] = useState(initialData);
+
   console.log(initialData);
+
+  const handleDelete = (id) => {
+    console.log("delete", id);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+
+              // ✅ UI update
+              const remaining = users.filter((user) => user._id !== id);
+              setUsers(remaining);
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div className="overflow-x-auto">
       <table className="table">
-        {/* head */}
         <thead>
           <tr>
+            <th>No</th>
             <th>Name</th>
             <th>Address</th>
             <th>Phone</th>
             <th></th>
           </tr>
         </thead>
+
         <tbody>
-          {initialData.map((user) => (
+          {users.map((user, index) => (
             <tr key={user._id}>
+              <th>{index + 1}</th>
               <td>
                 <div className="flex items-center gap-3">
                   <div className="avatar">
                     <div className="mask mask-squircle h-12 w-12">
-                      <img
-                        src={user.photo}
-                        alt="Avatar Tailwind CSS Component"
-                      />
+                      <img src={user.photo} alt="User Avatar" />
                     </div>
                   </div>
                   <div>
@@ -35,13 +72,19 @@ const Users = () => {
                   </div>
                 </div>
               </td>
-              <td>
-                {user.address}
-                <br />
-              </td>
+
+              <td>{user.address}</td>
               <td>{user.phoneNumber}</td>
+
               <th>
-                <button className="btn btn-ghost btn-xs">details</button>
+                <button className="btn btn-xs">view</button>
+                <button
+                  onClick={() => handleDelete(user._id)}
+                  className="btn btn-xs"
+                >
+                  delete
+                </button>
+                <button className="btn btn-xs">edit</button>
               </th>
             </tr>
           ))}
